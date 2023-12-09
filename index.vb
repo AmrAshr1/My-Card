a434aaff638ef1ecd76353089889fcb896b7288b
AmrAshr1 <153360431+AmrAshr1@users.noreply.github.com>
2023-12-09 16:22:36 +0200

    Add files via upload

---
+++ index.vb
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title> Amr Ashry </title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+</head>
+
+<body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover"
+	style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+
+	<div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+
+		<div id="profile"
+			class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+
+			<div class="p-4 md:p-12 text-center lg:text-left">
+				
+				<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
+					style="background-image: url('img/amrashry.jpg')">
+				</div>
+				<h1 class="text-3xl font-bold pt-8 lg:pt-0">Amr Ashry Abd-elmalik </h1>
+				<div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
+				<p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg
+						class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20">
+						<path
+							d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
+					</svg> accountant</p>
+				<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg
+						class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20">
+						<path
+							d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z" />
+					</svg>01010617878</p>
+
+				<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg
+						class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20">
+						<path
+							d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" />
+					</svg>Giza 6 of October</p>
+
+
+				<p class="pt-8 text-sm">A newly graduated accountant from the professional and professional skills in working in the organization and the profession in the job work and using my skills that qualify me the right to make the right choice for this position</p>
+
+				<div class="pt-12 pb-8">
+					<a href="./img/almeddin ahmed98 c.v -.pdf">
+						<button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
+							Download cv
+						</button></a>
+				</div>
+
+				<div class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
+					<a class="link" href="https://www.facebook.com/profile.php?id=100007731990802&mibextid=ZbWKwL"
+						data-tippy-content="Follow me on Facebook"><svg
+							class="h-6 fill-current text-gray-600 hover:text-green-700" role="img" viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor">
+							<title>Facebook</title>
+							<path
+								d="M22.676 0H1.324C.593 0 0 .593 0 1.324v21.352C0 23.408.593 24 1.324 24h11.494v-9.294H9.689v-3.621h3.129V8.41c0-3.099 1.894-4.785 4.659-4.785 1.325 0 2.464.097 2.796.141v3.24h-1.921c-1.5 0-1.792.721-1.792 1.771v2.311h3.584l-.465 3.63H16.56V24h6.115c.733 0 1.325-.592 1.325-1.324V1.324C24 .593 23.408 0 22.676 0" />
+						</svg></a>
+					<a class="link"
+						href="https://instagram.com/almeddin_ahmed?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D"
+						data-tippy-content="Follow me on instagram"><svg
+							class="h-6 fill-current text-gray-600 hover:text-green-700" role="img" viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg">
+							<title>Instagram</title>
+							<path
+								d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z" />
+						</svg></a>
+
+
+					<a class="link" href="https://twitter.com/AlmEddinAhmed1?t=jR2sg1vw1cd4q6oivmoX5Q&s=09"
+						data-tippy-content="Follow me on twitter"><svg
+							class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+							xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor"
+							class="bi bi-whatsapp" viewBox="0 0 16 16" id="IconChangeColor">
+							<title>twitter</title>
+							<path
+								d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z">
+						</svg></a>
+
+					<a class="link"
+						href="https://www.linkedin.com/in/%D8%B9%D9%84%D9%85-%D8%A7%D9%84%D8%AF%D9%8A%D9%86-%D8%A7%D8%AD%D9%85%D8%AF-0741a4172"
+						data-tippy-content="Follow me on linkedin"><svg
+							class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+							xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor"
+							class="bi bi-whatsapp" viewBox="0 0 16 16" id="IconChangeColor">
+							<title>linkedin</title>
+							<path
+								d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z">
+						</svg></a>
+
+					<a class="link"
+						href="https://api.whatsapp.com/send/?phone=201010617878&text&type=phone_number&app_absent=0"
+						data-tippy-content="Send me on Whatsapp"><svg
+							class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+							xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor"
+							class="bi bi-whatsapp" viewBox="0 0 16 16" id="IconChangeColor">
+							<title>whatsapp</title>
+							<path
+								d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+						</svg></a>
+
+				</div>
+			</div>
+
+		</div>
+
+		<div class="w-full lg:w-2/5">
+			<img src="img/eilm aldiyn.jfif" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
+
+		</div>
+
+
+		<div class="absolute top-0 right-0 h-12 w-18 p-4">
+			<button class="js-change-theme focus:outline-none">🌙</button>
+		</div>
+
+	</div>
+
+	<script src="https://unpkg.com/popper.js@1/dist/umd/popper.min.js"></script>
+	<script src="https://unpkg.com/tippy.js@4"></script>
+	<script src="app.js"></script>
+	<script>
+		//Init tooltips
+		
+	</script>
+
+</body>
+
+</html>
